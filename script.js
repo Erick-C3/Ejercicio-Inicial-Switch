ab7aaaaab66c131b1dc9dfd27a45317e321a69d2
Erick <prueba>
2023-05-05 19:40:44 -0300

    termine con opciones de colores para el texto

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const pagina = document.querySelector("body");
 const selectOpcionColor = document.querySelector("select");
 
+const COLOR_1 = "color 1";
+
+const COLOR_2 = "color 2";
+const COLOR_3 = "color 3";
 function cambioEstetica(){
     console.log(selectOpcionColor.value);
 
     switch (selectOpcionColor.value ) {
-        case "color 1":
+        case COLOR_1:
             pagina.style.color = "white";
             pagina.style.backgroundColor = "black";
             break;
-    
+        case COLOR_2:
+            pagina.style.color = "violet";
+            pagina.style.backgroundColor = "orange";
+            break;
+        case COLOR_3:
+            pagina.style.color = "pink";
+            pagina.style.backgroundColor = "blue";
+            break;
         default:
             alert("ERROR");
             break;
