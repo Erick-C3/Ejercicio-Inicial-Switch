71d8777efac290540f0ed3d9f33ad40da62b2b81
Erick <erick.nahuelpaiva.001@gmail.com>
2023-05-06 15:35:52 -0300

    agregadas constantes para buenas practicass

--- script.js
+++ script.js
@@ -7,34 +7,42 @@ const OPCION_2 = "color 2";
 const OPCION_3 = "color 3";
 
 const COLOR_OPC_1 = "white";
+const COLOR_OPC_2 = "violet";
+const COLOR_OPC_3 = "pink";
 
 const IMG_OPC_1 = "https://www.biografiasyvidas.com/monografia/chaplin/fotos/chaplin340a.jpg";
 const IMG_OPC_2 = "https://http2.mlstatic.com/D_NQ_NP_821054-MLA25926284569_082017-O.jpg";
 const IMG_OPC_3 = "https://www.proyectacolor.cl/wp-content/uploads/2008/12/ros3.jpg";
 
 
+const COLOR_FONDO_OPC_1 = "black";
+const COLOR_FONDO_OPC_2 = "orange";
+const COLOR_FONDO_OPC_3 = "blue";
+
+const MSJ_ERROR = "Error!";
+
 function cambioEstetica(){
     switch (selectOpcionColor.value ) {
         case OPCION_1:
             pagina.style.color = COLOR_OPC_1;
-            pagina.style.backgroundColor = "black";
+            pagina.style.backgroundColor = COLOR_FONDO_OPC_1;
             selectOpcionColor.style.color = COLOR_OPC_1;
             imgColor.src = IMG_OPC_1;
             break;
         case OPCION_2:
-            pagina.style.color = "violet";
-            pagina.style.backgroundColor = "orange";
+            pagina.style.color = COLOR_OPC_2;
+            pagina.style.backgroundColor = COLOR_FONDO_OPC_2;
             imgColor.src = IMG_OPC_2;
-            selectOpcionColor.style.color = "violet";
+            selectOpcionColor.style.color = COLOR_OPC_2;
             break;
         case OPCION_3:
-            pagina.style.color = "pink";
-            selectOpcionColor.style.color = "pink";
+            pagina.style.color = COLOR_OPC_3;
+            selectOpcionColor.style.color = COLOR_OPC_3;
             imgColor.src = IMG_OPC_3;
-            pagina.style.backgroundColor = "blue";
+            pagina.style.backgroundColor = COLOR_FONDO_OPC_3;
             break;
         default:
-            alert("ERROR");
+            alert(MSJ_ERROR);
             break;
     }
 }
