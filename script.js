b8b9d0386243292e588e9e1ac12b58b22000c0e7
Erick <prueba>
2023-05-05 21:08:55 -0300

    mejoras esteticas

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 const pagina = document.querySelector("body");
 const selectOpcionColor = document.querySelector("select");
+const imgColor = document.querySelector("img");
 
-const COLOR_1 = "color 1";
+const OPCION_1 = "color 1";
+const OPCION_2 = "color 2";
+const OPCION_3 = "color 3";
 
-const COLOR_2 = "color 2";
-const COLOR_3 = "color 3";
+const COLOR_OPC_1 = "white";
+
+const IMG_OPC_1 = "https://www.biografiasyvidas.com/monografia/chaplin/fotos/chaplin340a.jpg";
+const IMG_OPC_2 = "https://http2.mlstatic.com/D_NQ_NP_821054-MLA25926284569_082017-O.jpg";
+const IMG_OPC_3 = "https://www.proyectacolor.cl/wp-content/uploads/2008/12/ros3.jpg";
+
+
 function cambioEstetica(){
-    console.log(selectOpcionColor.value);
-
     switch (selectOpcionColor.value ) {
-        case COLOR_1:
-            pagina.style.color = "white";
+        case OPCION_1:
+            pagina.style.color = COLOR_OPC_1;
             pagina.style.backgroundColor = "black";
+            selectOpcionColor.style.color = COLOR_OPC_1;
+            imgColor.src = IMG_OPC_1;
             break;
-        case COLOR_2:
+        case OPCION_2:
             pagina.style.color = "violet";
             pagina.style.backgroundColor = "orange";
+            imgColor.src = IMG_OPC_2;
+            selectOpcionColor.style.color = "violet";
             break;
-        case COLOR_3:
+        case OPCION_3:
             pagina.style.color = "pink";
+            selectOpcionColor.style.color = "pink";
+            imgColor.src = IMG_OPC_3;
             pagina.style.backgroundColor = "blue";
             break;
         default:
